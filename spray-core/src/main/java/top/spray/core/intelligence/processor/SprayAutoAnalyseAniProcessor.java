e3af32716e48f704e613caebeba44afa7bcbee11
LinJieBin <aa1274791421.aa@qq.com>
2024-07-03 11:07:48 +0800

    feat spray auto analyse support

--- spray-core/src/main/java/top/spray/core/intelligence/processor/SprayAutoAnalyseAniProcessor.java
+++ spray-core/src/main/java/top/spray/core/intelligence/processor/SprayAutoAnalyseAniProcessor.java
@@ -12,14 +12,15 @@ import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.ListBuffer;
 import com.sun.tools.javac.util.Names;
 import top.spray.core.intelligence.annotation.SprayAutoAnalyse;
+import top.spray.core.intelligence.handler.SprayAutoAnalyserResolveHandler;
+import top.spray.core.util.SprayServiceUtil;
 
 import javax.annotation.processing.*;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.block;
 import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.parameters;
@@ -50,30 +51,39 @@ public class SprayAutoAnalyseAniProcessor extends AbstractProcessor {
         // 拿到被注解标注的所有的类
         Set<? extends Element> elementsAnnotatedWith = roundEnv.getElementsAnnotatedWith(SprayAutoAnalyse.class);
         elementsAnnotatedWith.forEach(element -> {
+            Iterator<SprayAutoAnalyserResolveHandler> iterator = ServiceLoader.load(SprayAutoAnalyserResolveHandler.class).iterator();
+            while (iterator.hasNext()) {
+                SprayAutoAnalyserResolveHandler sprayAutoAnalyserResolveHandler = iterator.next();
+                if (sprayAutoAnalyserResolveHandler.canHandle(element)){
+                    sprayAutoAnalyserResolveHandler.handle(element, roundEnv, messager, javacTrees, treeMaker, names);
+                    break;
+                }
+            }
+
             // 得到类的抽象树结构
             JCTree tree = javacTrees.getTree(element);
             // 遍历类，对类进行修改
-            tree.accept(new TreeTranslator(){
-                @Override
-                public void visitClassDef(JCTree.JCClassDecl jcClassDecl) {
-                    List<JCTree.JCVariableDecl> jcVariableDeclList = List.nil();
-                    // 在抽象树中找出所有的变量
-                    for(JCTree jcTree: jcClassDecl.defs){
-                        if (jcTree.getKind().equals(Tree.Kind.VARIABLE)){
-                            JCTree.JCVariableDecl jcVariableDecl = (JCTree.JCVariableDecl)jcTree;
-                            jcVariableDeclList = jcVariableDeclList.append(jcVariableDecl);
-                        }
-                    }
-                    
-                    // 对于变量进行生成方法的操作
-                    for (JCTree.JCVariableDecl jcVariableDecl : jcVariableDeclList) {
-                        messager.printMessage(Diagnostic.Kind.NOTE, jcVariableDecl.getName() + " has been processed");
-                        jcClassDecl.defs = jcClassDecl.defs.prepend(makeSetterMethodDecl(jcVariableDecl));
+            tree.accept(new TreeTranslator() {
+                            @Override
+                            public void visitClassDef(JCTree.JCClassDecl jcClassDecl) {
+                                List<JCTree.JCVariableDecl> jcVariableDeclList = List.nil();
+                                // 在抽象树中找出所有的变量
+                                for (JCTree jcTree : jcClassDecl.defs) {
+                                    if (jcTree.getKind().equals(Tree.Kind.VARIABLE)) {
+                                        JCTree.JCVariableDecl jcVariableDecl = (JCTree.JCVariableDecl) jcTree;
+                                        jcVariableDeclList = jcVariableDeclList.append(jcVariableDecl);
+                                    }
+                                }
 
-                        jcClassDecl.defs = jcClassDecl.defs.prepend(makeGetterMethodDecl(jcVariableDecl));
-                    }
+                                // 对于变量进行生成方法的操作
+                                for (JCTree.JCVariableDecl jcVariableDecl : jcVariableDeclList) {
+                                    messager.printMessage(Diagnostic.Kind.NOTE, jcVariableDecl.getName() + " has been processed");
+                                    jcClassDecl.defs = jcClassDecl.defs.prepend(makeSetterMethodDecl(jcVariableDecl));
 
-
+                                    jcClassDecl.defs = jcClassDecl.defs.prepend(makeGetterMethodDecl(jcVariableDecl));
+                                }
+                            }
+                        });
         // 生成返回对象
         JCTree.JCExpression methodType = treeMaker.Type(new Type.JCVoidType());
 
