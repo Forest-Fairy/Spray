6b27282c9da3d5a541d937d5b6bada21bd1f6403
ForestFairy <aa1274791421.aa@qq.com>
2025-03-24 18:09:42 +0800

    feat SprayProgramingWorkflow tmp

---
+++ spray-engine/spray-process/src/main/java/top/spray/engine/process/processor/builder/SprayComputer.java
@@ -0,0 +1,7 @@
+package top.spray.engine.process.processor.builder;
+
+public class SprayComputer {
+    private SprayComputer(){
+
+    }
+}
--- spray-engine/spray-process/src/main/java/top/spray/engine/process/processor/builder/SprayProcessBuilder.java
+++ spray-engine/spray-process/src/main/java/top/spray/engine/process/processor/builder/SprayProcessBuilder.java
@@ -1,6 +1,27 @@
 package top.spray.engine.process.processor.builder;
 
 
+import top.spray.engine.process.processor.dispatch.coordinate.meta.SprayProcessCoordinatorMeta;
+import top.spray.engine.process.processor.execute.step.meta.SprayProcessExecuteStepMeta;
+
 public class SprayProcessBuilder {
+
+    private final SprayProcessCoordinatorMeta coordinatorMeta;
+
+    public SprayProcessBuilder(SprayProcessCoordinatorMeta coordinatorMeta) {
+        this.coordinatorMeta = coordinatorMeta;
+    }
+
+    public SprayProcessBuilder computeWith(SprayComputer computer) {
+
+    }
+
+
+    private static class SprayStepDescriptor {
+        private SprayProcessExecuteStepMeta stepMeta;
+        private SprayStepDescriptor(SprayComputer computer) {
+            this.stepMeta = stepMeta;
+        }
+    }
 
 }
